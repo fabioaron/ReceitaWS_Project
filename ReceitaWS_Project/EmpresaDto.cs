2bf86db898cff3f4e3063e170b2853fa41ccacf8
fabioaron <fabioaronsantos@gmail.com>
2025-04-22 22:48:07 -0300

    Adicionar arquivos de projeto.

---
+++ ReceitaWS_Project/EmpresaDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ReceitaWS_Project 
+{ 
+public class EmpresaDto
+{
+    [JsonPropertyName("cnpj")]
+    public string Cnpj { get; set; }
+
+    [JsonPropertyName("nome")]
+    public string Nome { get; set; }
+
+    [JsonPropertyName("uf")]
+    public string Uf { get; set; }
+
+    // Adicione outras propriedades conforme os dados retornados pela API.
+}
+}
